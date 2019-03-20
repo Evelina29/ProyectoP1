e8d1007b123959eb77f012c273bfb8ca9f6f3c72
Evelina Villa <Evelina Villa@LAPTOP-UBL6F06J>
2019-03-20 15:01:21 -0400

    d

--- ProyectoFinalP1/src/logical/Componente.java
+++ ProyectoFinalP1/src/logical/Componente.java
@@ -2,12 +2,62 @@ package logical;
 
 public abstract class Componente {
 	
+	
 	protected String modelo;
 	protected String marca;
 	protected String tipo;
 	protected float precio;
 	protected int cantDisponible;
 	protected String serie;
+	
+	public Componente(String modelo, String marca, String tipo, float precio, int cantDisponible, String serie) {
+		super();
+		this.modelo = modelo;
+		this.marca = marca;
+		this.tipo = tipo;
+		this.precio = precio;
+		this.cantDisponible = cantDisponible;
+		this.serie = serie;
+	}
+	
+	
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getCantDisponible() {
+		return cantDisponible;
+	}
+	public void setCantDisponible(int cantDisponible) {
+		this.cantDisponible = cantDisponible;
+	}
+	public String getSerie() {
+		return serie;
+	}
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
 	
 
 }
