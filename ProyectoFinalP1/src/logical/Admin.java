c630f495cb5670c7e2ac0ffc04e73c9351885d2e
lele <lele@192.168.0.115>
2019-03-20 16:05:52 -0400

    correccion de la parte Admin

--- ProyectoFinalP1/src/logical/Admin.java
+++ ProyectoFinalP1/src/logical/Admin.java
@@ -4,21 +4,34 @@ public class Admin extends Person {
 	
 	
 	private static Tienda[] miTienda;
+	private static int cantTienda;
 
 	static {
 		miTienda = new  Tienda[10];
+		cantTienda =0;
+		
 	}
 	
 	
 	public Admin(String nombre, String apellido, String iD, String direccion) {
 		super(nombre, apellido, iD, direccion);
-		// TODO Auto-generated constructor stub
+		
 		
 		
 	}
 	
-	public void crearTienda() {
+	public void crearTienda(Tienda tienda) {
+		miTienda[cantTienda]=tienda;
+		cantTienda++;
 		
+	}
+
+	public static Tienda[] getMiTienda() {
+		return miTienda;
+	}
+
+	public static int getCantTienda() {
+		return cantTienda;
 	}
 
 }
