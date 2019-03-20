ad5ca0a5ccddc07b6cf19b19f956d186a3c443c9
Evelina Villa <Evelina Villa@LAPTOP-UBL6F06J>
2019-03-20 14:53:05 -0400

    s

--- ProyectoFinalP1/src/logical/Componente.java
+++ ProyectoFinalP1/src/logical/Componente.java
@@ -1,5 +1,13 @@
 package logical;
 
 public abstract class Componente {
+	
+	protected String modelo;
+	protected String marca;
+	protected String tipo;
+	protected float precio;
+	protected int cantDisponible;
+	protected String serie;
+	
 
 }
