46395f0c8f6c5be5a43a6b6e999d54d4139a4eaa
Windows 10 <Windows 10@Windows-10-PRO>
2019-03-20 15:49:05 -0400

    Cliente y PC

---
+++ ProyectoFinalP1/src/logical/Cliente.java
@@ -0,0 +1,12 @@
+package logical;
+
+public class Cliente extends Person {
+	
+	
+
+	public Cliente(String nombre, String apellido, String iD, String direccion) {
+		super(nombre, apellido, iD, direccion);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ProyectoFinalP1/src/logical/PC.java
@@ -0,0 +1,7 @@
+package logical;
+
+public class PC {
+	
+	
+
+}
