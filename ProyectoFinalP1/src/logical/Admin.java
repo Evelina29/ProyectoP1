5aec1c5865b65523f34d78293b08423072cc3983
Windows 10 <Windows 10@Windows-10-PRO>
2019-03-20 15:28:11 -0400

    clase person clase Tienda Clase Admin

---
+++ ProyectoFinalP1/src/logical/Admin.java
@@ -0,0 +1,24 @@
+package logical;
+
+public class Admin extends Person {
+	
+	
+	private Tienda[] miTienda;
+
+	/*static {
+		miTienda = Tienda[10];
+	}*/
+	
+	
+	public Admin(String nombre, String apellido, String iD, String direccion) {
+		super(nombre, apellido, iD, direccion);
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+	
+	public void crearTienda() {
+		
+	}
+
+}
