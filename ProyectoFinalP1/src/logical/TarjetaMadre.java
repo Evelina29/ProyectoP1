c32affd2dfdc3d1d5d93d64468d2bf655b5d1aec
Evelina Villa <Evelina Villa@LAPTOP-UBL6F06J>
2019-03-20 15:27:32 -0400

    sre

--- ProyectoFinalP1/src/logical/TarjetaMadre.java
+++ ProyectoFinalP1/src/logical/TarjetaMadre.java
@@ -2,9 +2,23 @@ package logical;
 
 public class TarjetaMadre extends Componente{
 
-	public TarjetaMadre(String modelo, String marca, String tipo, float precio, int cantDisponible, String serie) {
+	private String conexiones;
+	
+	
+	public TarjetaMadre(String modelo, String marca, String tipo, float precio, int cantDisponible, String serie, String conexiones) {
 		super(modelo, marca, tipo, precio, cantDisponible, serie);
 		// TODO Auto-generated constructor stub
+		
+		this.conexiones = conexiones;
+		
+	}
+
+	public String getConexiones() {
+		return conexiones;
+	}
+
+	public void setConexiones(String conexiones) {
+		this.conexiones = conexiones;
 	}
 
 }
