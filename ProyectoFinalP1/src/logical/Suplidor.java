93f5de427aeb5b7c9e8ac852d33dea2cac80385d
Windows 10 <Windows 10@Windows-10-PRO>
2019-03-20 15:43:41 -0400

    Vendedor y Suplidor

--- ProyectoFinalP1/src/logical/Suplidor.java
+++ ProyectoFinalP1/src/logical/Suplidor.java
@@ -6,9 +6,24 @@ public class Suplidor extends Person {
 	
 	
 
-	public Suplidor(String nombre, String apellido, String iD, String direccion) {
+	public Suplidor(String nombre, String apellido, String iD, String direccion, String Pais) {
 		super(nombre, apellido, iD, direccion);
 		// TODO Auto-generated constructor stub
+		this.Pais=Pais;
 	}
+
+
+
+	public String getPais() {
+		return Pais;
+	}
+
+
+
+	public void setPais(String pais) {
+		Pais = pais;
+	}
+	
+	
 
 }
---
+++ ProyectoFinalP1/src/logical/Vendedor.java
@@ -0,0 +1,12 @@
+package logical;
+
+public class Vendedor extends Person {
+	
+
+
+	public Vendedor(String nombre, String apellido, String iD, String direccion) {
+		super(nombre, apellido, iD, direccion);
+		// TODO Auto-generated constructor stub
+	}
+
+}
